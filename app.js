53e8930b9c8c9a9b1e0c5d7670f1d435e7321762
CorvusCodex <56726555+CorvusCodex@users.noreply.github.com>
2023-05-10 11:52:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 
 process.title = "Bitcoin Bruteforce";
 
+//Creaded by: Corvus Codex
+//Github: https://github.com/CorvusCodex/
+//Licence : MIT License
+
+//Support my work:
+//BTC: bc1q7wth254atug2p4v9j3krk9kauc0ehys2u8tgg3
+//ETH & BNB: 0x68B6D33Ad1A3e0aFaDA60d6ADf8594601BE492F0
+//Buy me a coffee: https://www.buymeacoffee.com/CorvusCodex
+
+
 // Importing required modules
 const CoinKey = require('coinkey');
 const fs = require('fs');
